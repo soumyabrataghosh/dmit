5c2522d8804410982acc0d4d038cd85e35e073a4
Soumyabrata Ghosh <soumyabrata.ghosh@uni.lu>
2022-11-02 11:38:57 +0100

    Deploy variable level

--- dashboard.py
+++ dashboard.py
@@ -3,8 +3,13 @@ import pandas as pd
 from pyparsing import col
 import streamlit as st
 import json
-st.set_page_config(layout='wide')
-
+#st.set_page_config(layout='wide')
+st.set_page_config(
+    page_title="LIMT - Variable selection and export",
+    page_icon="💾",
+    layout="wide",
+    initial_sidebar_state="expanded",
+)
 # %%
 #df=pd.read_excel('/Users/soumyabrata.ghosh/code-repo/dmit/CONVINCE-ORCHESTRA + PREDICOVID + IGSS data dictionary 25-OCT-2022.xlsx',sheet_name='CONVINCE-ORCHESTRA')
 # %% Read from Google Sheet
@@ -16,9 +21,21 @@ df_gs = pd.read_csv(url)
 # %%
 styl = f'''<style>
 .element-container {{
-    max-height: 300px;
+    max-height: 500px;
     overflow-x: hidden;
     overflow-y: scroll;
+}}
+button.css-firdtp {{
+    position: fixed;
+    bottom: 10%;
+    right: 10%;
+    z-index: 9999;
+}}
+.stDownloadButton button {{
+    position: fixed;
+    bottom: 5%;
+    right: 10%;
+    z-index: 9999;   
 }}
 
 </style>
@@ -28,31 +45,49 @@ st.markdown(styl, unsafe_allow_html=True)
 # df_gs
 # %%
 #st.write(df_gs)
-st.subheader('Variable selection for data export')
-top_cols1, top_cols2= st.columns(2)
+st.sidebar.title('LIMT')
+st.sidebar.header('LCSB Information Minimisation Toolkit (Alpha)')
+st.sidebar.subheader('Variable selection for data export')
+top_cols1, top_cols2= st.columns([2,1])
+
 with top_cols1:
-    st.code('Click "Explore variables" to view variables in each category.')
-    container = st.container()
-with top_cols2:
-    st.code('Select the category checkboxes, click Submit, then copy the json below.')
-    container_json = st.container()
-# %%
-st.code('Variable categories:')
-selected_cat = {}
-cols = st.columns(5)
-cat_list = list(df_gs['categories'].dropna().unique())
+    st.markdown('Click `Explore variables` to explore and select variables in each category. After selction, click on `Submit` Button and then click `Download JSON` button.')
+    #container = st.container()``
 
-with st.form(key='my_form'):
-    cat_no = 1
-    for cat in cat_list:
-        with cols[int(cat_no/(int(len(cat_list)/5)+1))]:
-            selected_cat[cat]=st.checkbox(cat)
-            if st.button('Explore variables',key=cat):
+    # %%
+    selected_cat = {}
+    #cols = st.columns(2)
+    cat_list = list(df_gs['categories'].dropna().unique())
+
+    with st.form(key='my_form'):
+        cat_no = 1
+        for cat in cat_list:
+            #with cols[int(cat_no/(int(len(cat_list)/2)+1))]:
+            selected_cat[cat]={'all_var':st.checkbox(cat)}
+            #if st.button('Explore variables',key=cat):
+            with st.expander('Explore variables'):
+                container = st.container()
                 container.write("Category: "+str(cat))
-                container.table(df_gs[df_gs['categories'] == cat]['question'])
-        cat_no = cat_no + 1
-    #with cols[-1]:
-    st.form_submit_button('Submit')
-container_json.code(json.dumps(selected_cat, indent = 4) )
-#container_json.json(selected_cat)
+                var_list = df_gs[df_gs['categories'] == cat][['question','CON-ORC_uui']].dropna().apply(tuple, axis=1)
+                for var_item in var_list:
+                    selected_cat[cat][var_item[1]]=container.checkbox(var_item[0],key=str(cat)+str(var_item[1]))
+                
 
+            cat_no = cat_no + 1
+        #with cols[-1]:
+        st.form_submit_button('Submit', type="primary")
+    json_string = json.dumps(selected_cat, indent = 4)
+    st.download_button(
+        label="Download JSON",
+        file_name="data.json",
+        mime="application/json",
+        data=json_string,
+        key = "jsondown",
+    )
+
+#container_json.json(selected_cat)
+with top_cols2:
+    container_json = st.container()
+    container_json.code(json_string)
+
+st.sidebar.markdown('Bug report and contact: soumyabrata.ghosh@uni.lu')
